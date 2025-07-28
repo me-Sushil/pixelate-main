c42354844f6e76e85d33bc23f8cf47456f636e9e
me-Sushil <girbahadurbk123@gmail.com>
2025-07-28 21:42:38 +0545

    create row and tabe data dynamically using js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Your code here
+//npm start
+
+let table = document.getElementById("table");
+
+function makeRow() {
+  let row = document.createElement("tr");
+  for(let i=0; i<20; i++){
+    let td = document.createElement("td");
+    row.appendChild(td);
+  }
+  table.appendChild(row);
+}
+makeRow();
+makeRow();
+makeRow();
+makeRow();
+makeRow();
+makeRow();
+makeRow();
+makeRow();
+makeRow();
