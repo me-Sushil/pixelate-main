296171a8679c841183bcfe5d4ca5c6422668caea
me-Sushil <girbahadurbk123@gmail.com>
2025-07-29 03:01:51 +0545

    add,  mouseover, mouseup, mousedown

--- script.js
+++ script.js
@@ -3,22 +3,45 @@
 
 let table = document.getElementById("table");
 let add = document.getElementById("add-row");
+
 function makeRow() {
   let row = document.createElement("tr");
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 610; i++) {
     let td = document.createElement("td");
     row.appendChild(td);
   }
   table.appendChild(row);
 }
-makeRow();
-makeRow();
-makeRow();
-makeRow();
+
+let count = 250;
+function makeTable(){
+    while(count > 0){
+        count--;
+        makeRow();
+    }
+}
+
+makeTable()
+
+
+table.addEventListener("mouseup", ()=>{
+    isMouseDown = false;
+});
 
 add.addEventListener("click", makeRow);
 table.addEventListener("click", colorize);
 
+
+let isMouseDown = false;
+table.addEventListener("mousedown", (event)=>{
+    isMouseDown = true;
+    //colorize(event);
+});
+let mouseover = table.addEventListener("mouseover", (event)=>{
+    if(isMouseDown){
+        colorize(event);
+    }
+});
 
 let choseColor = "red";
 function colorize(event) {
@@ -35,3 +58,4 @@ let select = document.getElementById("select");
 select.addEventListener("change", (event)=>{
     choseColor = event.target.value;   
 })
+
