667fc3b46d4d09735b472a175ff7cd0e0d489036
me-Sushil <girbahadurbk123@gmail.com>
2025-07-28 21:57:48 +0545

    handel event

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //npm start
 
 let table = document.getElementById("table");
-
+let add = document.getElementById("add-row");
 function makeRow() {
   let row = document.createElement("tr");
   for(let i=0; i<20; i++){
@@ -13,10 +13,10 @@ function makeRow() {
 }
 makeRow();
 makeRow();
-makeRow();
-makeRow();
-makeRow();
-makeRow();
-makeRow();
-makeRow();
-makeRow();
+
+add.addEventListener("click", makeRow)
+table.addEventListener("click", clicked)
+
+function clicked(event){
+  console.log("clicked");
+}
